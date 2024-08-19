3e1c8783d0140656caa34fea2ac07c8c1250f191
yjeongyjeong <dbswjd4991@naver.com>
2024-08-19 19:44:05 +0900

    build : 빌드파일 설정 및 application.yml에서 datasource url 추가

---
+++ src/test/java/me/yjeong/springbootdeveloper/SpringbootDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yjeong.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
