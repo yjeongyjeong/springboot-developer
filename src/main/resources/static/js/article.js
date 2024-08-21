013cfe5d5d36fab1837ab9a33477ff9351bff213
yjeongyjeong <dbswjd4991@naver.com>
2024-08-21 17:05:28 +0900

    feat: blog 게시글 삭제 view 기능 생성

---
+++ src/main/resources/static/js/article.js
@@ -0,0 +1,15 @@
+//delete
+const deleteButton = document.getElementById('delete-btn');
+
+if (deleteButton){
+    deleteButton.addEventListener('click', ev => {
+        let id = document.getElementById('article-id').value;
+        fetch(`/api/articles/${id}`, {
+            method : 'DELETE'
+        })
+            .then(() => {
+                alert('삭제가 완료되었습니다.');
+                location.replace('/articles');
+            });
+    });
+}
