8d9ce7b27224a9e2dcb80820dbe9c4df9daf7030
yjeongyjeong <dbswjd4991@naver.com>
2024-08-22 16:38:37 +0900

    feat: 회원 도메인 생성 및 security 관련 클래스 생성

---
+++ src/main/java/me/yjeong/springbootdeveloper/domain/User.java
@@ -0,0 +1,70 @@
+package me.yjeong.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@Entity
+@Getter
+@NoArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Builder
+    public User(String email, String password, String auth){
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override   // 권한 반환
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override   // 사용자의 id를 반환(고유한 값)
+    public String getUsername() {
+        return email;
+    }
+
+    @Override   // 계정 만료 여부 반환
+    public boolean isAccountNonExpired() {
+//        return UserDetails.super.isAccountNonExpired();
+        return true;
+    }
+
+    @Override   // 계정 잠금 여부 반환
+    public boolean isAccountNonLocked() {
+//        return UserDetails.super.isAccountNonLocked();
+        return true;
+    }
+
+    @Override   // 패스워드의 만료 여부 반환
+    public boolean isCredentialsNonExpired() {
+//        return UserDetails.super.isCredentialsNonExpired();
+        return true;
+    }
+
+    @Override   // 계정 사용 가능 여부
+    public boolean isEnabled() {
+//        return UserDetails.super.isEnabled();
+        return true;
+    }
+}
